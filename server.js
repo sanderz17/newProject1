6633127aea5bee7b65200c42baee05af77c8eeef
philip <philip31th@gmail.com>
2023-03-24 21:07:45 +0800

    added cors and added  json express function

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const port = 3000;
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Web REST API" });
+});
+
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`Web API is running on port ${port}`)
